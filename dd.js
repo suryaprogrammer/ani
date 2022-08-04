289ad2ddb3270e54ca592d0e379af8e75e79c813
surya sundar <90978080+suryaprogrammer@users.noreply.github.com>
2022-08-04 11:32:46 +0530

    Add files via upload

---
+++ dd.js
@@ -0,0 +1,14 @@
+let gt = {
+    "animal" : {
+        1:"Animal1",
+        2:"Animal2",
+        3:"Animal3",
+        4:"Animal4",
+        5:"Animal5",
+        6:"Animal6",
+        7:"Animal7",
+        8:"Animal8"
+
+    }
+}
+
